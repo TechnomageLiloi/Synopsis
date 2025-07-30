4417b964508a99c99f5cbbed8cd6b90487e1e925
Anton Moskalenko <moskalenko.anton@gmail.com>
2025-07-30 22:35:11 +0300

    [0.00.006] Add index.

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+include_once __DIR__ . '/vendor/autoload.php';
+
+$private = json_decode(file_get_contents(__DIR__ . '/Config/Private.json'), true);
+
+$config = array_merge([
+    'title' => 'Synopsis',
+    'start' => 'Requests.layout();',
+    'scripts' => [
+    ],
+    'prefix' => ''
+], $private);
+
+$app = new \Liloi\Synopsis\Application($config);
+
+echo $app->compile();
